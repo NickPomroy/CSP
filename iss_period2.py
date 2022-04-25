1c15a57dac60db8f6952a500ef7f284b863a2d5b
NickPomroy <Nicholas.Pomroy@ptaaschool.net>
2022-04-25 10:21:37 -0500

    y

--- iss_period2.py
+++ iss_period2.py
@@ -1,3 +1,4 @@
+from logging import exception
 import tkinter as tk
 from turtle import onclick
 from PIL import Image, ImageTk
@@ -6,8 +7,25 @@ from io import BytesIO
 import json
 import config as cfg
 
+def get_iss_location(url:str):
+    try:
+        iss_request = urlopen(url)
+        iss_location = json.loads(iss_request.read())
+
+        print(iss_location)
+
+        longitude = iss_location['iss_position']['longitude']
+        latitude = iss_location['iss_position']['latitude']
+
+        return longitude, latitude
+    except Exception as e:
+        print(e)
+        root.quit()
+
 def refresh(x):
     pass
+
+
 
 # Window section
 root = tk.Tk()
@@ -18,6 +36,6 @@ display = tk.Label(root,font=('Arial 18 bold'),fg='red')
 display.grid(row=0,column=0)
 display.bind('<Button>',refresh)
 
-iss_location = get_iss_location('https://api.open-notify.org/iss-now.json')
+iss_location = get_iss_location('http://api.open-notify.org/iss-now.json')
 
 root.mainloop()
