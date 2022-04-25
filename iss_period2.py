1a0c0b6e71d28f859dc21e729f15d4d9899c657a
NickPomroy <Nicholas.Pomroy@ptaaschool.net>
2022-04-25 10:00:36 -0500

    y

--- iss_period2.py
+++ iss_period2.py
@@ -6,9 +6,18 @@ from io import BytesIO
 import json
 import config as cfg
 
+def refresh(x):
+    pass
+
 # Window section
 root = tk.Tk()
 root.title('ISS Location')
 root.geometry('600x600')
+
+display = tk.Label(root,font=('Arial 18 bold'),fg='red')
+display.grid(row=0,column=0)
+display.bind('<Button>',refresh)
+
+iss_location = get_iss_location('https://api.open-notify.org/iss-now.json')
 
 root.mainloop()
