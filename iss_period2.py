3eb0abe79730dc25085572cf199874fdad322298
NickPomroy <Nicholas.Pomroy@ptaaschool.net>
2022-04-25 09:52:29 -0500

    y

--- iss_period2.py
+++ iss_period2.py
@@ -5,3 +5,10 @@ from urllib.request import urlopen
 from io import BytesIO
 import json
 import config as cfg
+
+# Window section
+root = tk.Tk()
+root.title('ISS Location')
+root.geometry('600x600')
+
+root.mainloop()
